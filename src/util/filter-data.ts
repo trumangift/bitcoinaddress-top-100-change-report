96323971718633159afca6cf127b1930d0ac9dc6
Mr.liu <leo.liu@asiahr.com>
2019-07-04 10:18:53 +0800

    扩展到比特币前100地址爬虫

--- src/util/filter-data.ts
+++ src/util/filter-data.ts
@@ -2,18 +2,26 @@ const cheerio = require('cheerio');
 function filter(data: string): any {
     return new Promise((resolve, reject) => {
        var $ = cheerio.load(data);
-       const tbOneAddress_top19 = $('#tblOne').find('tbody tr td > a'); 
-       // const tblOne2 = $('#tblOne2').find('tbody tr td > a');
-       const tbOneBitcoinAmount_top19 = $('#tblOne').find('tbody tr');
+       let tbOneAddress_top19 = $('#tblOne').find('tbody tr td > a'); 
+       const tbOneAddress_top100 = $('#tblOne2').find('tbody tr td > a');
+       let tbOneBitcoinAmount_top19 = $('#tblOne').find('tbody tr');
+       let tbOneBitcoinAmount_top100 = $('#tblOne2').find('tbody tr');
        const addresses_top19 = tbOneAddress_top19.map(function(i: number, el: any) {
           return $(el).text();
        }).get();
+       const addresses_top100 = tbOneAddress_top100.map(function(i: number, el: any) {
+        return $(el).text();
+     }).get();
 
        const address_count_top19 = tbOneBitcoinAmount_top19.map(function(i: number, el: any) {
            const btc_count_text = $(el).find('td').eq(2).text();
            return btc_count_text.substr(0, btc_count_text.indexOf('BTC') -1 );
        }).get();
-       resolve([addresses_top19, address_count_top19]);
+       const address_count_top100 = tbOneBitcoinAmount_top100.map(function(i: number, el: any) {
+        const btc_count_text = $(el).find('td').eq(2).text();
+        return btc_count_text.substr(0, btc_count_text.indexOf('BTC') -1 );
+    }).get();
+       resolve([addresses_top19.concat(addresses_top100), address_count_top19.concat(address_count_top100)]);
     });
 }
 module.exports = {
